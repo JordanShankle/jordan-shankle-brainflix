85f30c9499a0b91cc80708d23ed8436c1d812d11
Jordan Shankle <ujshankle@gmail.com>
2023-01-11 17:27:41 -0600

    Delete components and refactor header styling.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,40 @@
 import './Header.scss';
 import BrainFlix from '../../assets/logo/BrainFlix-logo.svg';
-import SearchBar from '../SearchBar/SearchBar';
-import Upload from '../Upload/Upload';
-import ProfilePic from '../ProfilePic/ProfilePic';
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header className='header'>
-            
+
             <Link to='/'>
-               <div className="header__image-container">
-                    <img className="header__image" src={BrainFlix}
+                <div className="header__logo-container">
+                    <img className="header__logo" src={BrainFlix}
                         alt='BrainFlix logo' />
                 </div>
             </Link>
 
+            <div className='header__search'>
+                <form className='header__form'>
+                    <label className='search'>
+                        <input type='text' placeholder='Search' name='search' className='search__input' />
+                    </label>
+                </form>
 
-            <form className='header__form'>
-                {/* SearchBar Component */}
-                <SearchBar />
 
-                {/* ProfilePic Component */}
-                <ProfilePic />
-            </form>
+                <div className="header-profile header-profile--mobile">
+                    <div className="header-profile__image"></div>
+                </div>
 
-            {/* Upload Component */}
-            <Upload />
+            </div>
+
+            <Link to='/upload'>
+                <button className='button'>UPLOAD</button>
+            </Link>
+
+            <div className="header-profile--tablet">
+                <div className="header-profile__image"></div>
+            </div>
+
         </header>
     )
 };
