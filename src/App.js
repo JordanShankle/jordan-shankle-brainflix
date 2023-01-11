f3100bde872ebfee9dc53f6c67f290bb135a82b4
Jordan Shankle <ujshankle@gmail.com>
2023-01-11 17:53:42 -0600

    Add HomePage.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,30 @@
 // Sass
 import './App.scss';
-import { useState } from 'react';
+
+// React Modules
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 // Pages
 import UploadPage from './pages/UploadPage/UploadPage';
 
 
 // Components
+import HomePage from './pages/HomePage/HomePage';
 import Header from './components/Header/Header';
-import MainVideo from './components/MainVideo/MainVideo';
-import JoinConvo from './components/JoinConvo/JoinConvo';
-import Sidebar from './components/Sidebar/Sidebar';
-
-// Json Data for Videos 
-import videosDetails from './data/video-details.json';
-
-
 
 
 // App Component Function
 function App() {
-
-  // For the Comments
-  const [selectedVideoDetails, setSelectedVideoDetails] = useState(videosDetails[0])
-
-
-
-   // Click Event
-  const clickHandler = (id) => {
-
-    let selected = videosDetails.find(video => {
-      return video.id === id
-    })
-
-    setSelectedVideoDetails(selected)
-  }
-
 
   return (
     <Router>
       <Header />
 
       <Routes>
-        <Route path='/upload' element={<UploadPage selectedVideo={selectedVideoDetails}/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/upload' element={<UploadPage />} />
       </Routes>
-
-
-      {/* <MainVideo mainVideo={selectedVideoDetails} />
-      <JoinConvo selectedVideo={selectedVideoDetails} />
-      <Sidebar clickHandler={clickHandler} selectedVideo={selectedVideoDetails}  /> */}
-
 
     </Router>
   );
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+// Components
+import MainVideo from '../../components/MainVideo/MainVideo';
+import JoinConvo from '../../components/JoinConvo/JoinConvo';
+import Sidebar from '../../components/Sidebar/Sidebar';
+
+import videosDetails from '../../data/video-details.json';
+
+function HomePage() {
+
+ // For the Comments
+  const [selectedVideoDetails, setSelectedVideoDetails] = useState(videosDetails[0])
+  
+
+  // Click Event
+ const clickHandler = (id) => {
+
+   let selected = videosDetails.find(video => {
+     return video.id === id
+   })
+
+   setSelectedVideoDetails(selected)
+ }
+    
+    return (
+       <main>
+        
+        <MainVideo mainVideo={selectedVideoDetails} />
+        <JoinConvo selectedVideo={selectedVideoDetails} />
+        <Sidebar clickHandler={clickHandler} selectedVideo={selectedVideoDetails}  />
+
+       </main>
+    )
+}
+
+export default HomePage;
